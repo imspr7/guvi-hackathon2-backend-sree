7971336a6073a94f759d0bc9ad25483c9b033c9d
imspr7 <sreepathirajendran@gmail.com>
2023-08-12 00:13:25 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,93 @@
+var express = require('express');
+const { productModel } = require('../schema/indexschema');
+var { dbUrl } = require('../dbconfig/dbconfig');
+var mongoose = require('mongoose');
+var router = express.Router();
+mongoose.connect(dbUrl)
+
+
+/* GET home page. */
+router.get('/', async function (req, res) {
+  try {
+    let product = await productModel.find();
+    res.status(200).send({
+      product
+    })
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+      error
+    })
+  }
+
+});
+
+//inset product
+router.post('/add/product', async (req, res) => {
+  try {
+    let product = await productModel.findOne({ id: req.body.id })
+    if (!product) {
+      let product = await productModel.create(req.body);
+      res.status(200).send(product)
+    } else {
+      res.status(400).send({ message: "product Alreay existed" });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Error",
+      error
+    })
+  }
+})
+//update product details
+router.put('/product/edit/:id', async (req, res) => {
+  try {
+    console.log(req.params.id);
+    let product = await productModel.findOne({ _id: req.params.id })
+    if (product) {
+      product.productName = req.body.productName
+      product.categories=req.body.categories
+      product.image = req.body.image
+      product.model = req.body.model
+      product.price = req.body.price
+      await product.save()
+      console.log(product)
+      res.status(200).send({
+        product,
+        message: "product update successfully"
+      })
+    } else {
+      res.status(400).send({ message: "product not exit" })
+    }
+  } catch (error) {
+    res.status(500).send({ message: "internal server error" })
+  }
+
+})
+
+//delete product
+router.delete('/product/delete/:id', async (req, res) => {
+  try {
+    let product = await productModel.findOne({ _id: req.params.id })
+    if (product) {
+      let product = await productModel.deleteOne({ _id: req.params.id })
+      res.status(200).send({
+        message: "product Deleted Successfull!"
+      })
+    }
+    else {
+      res.status(400).send({ message: "product  Does Not Exists!" })
+    }
+
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+      error
+    })
+
+  }
+})
+
+
+
+module.exports = router;
---
+++ schema/indexschema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  productName: { type: String, required: true },
+  categories:{ type: String, required: true },
+  model:{type:String,required:true},
+  image:{type:String,required:true},
+  price:{type:Number,required:true}
+
+
+
+}
+);
+
+let productModel = mongoose.model('productAdmin', productSchema)
+module.exports = { productModel }
